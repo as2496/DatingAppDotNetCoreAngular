9bff60459f5aa8015f191d9a291f11c2ac4e2b36
Anushree <as2496@njit.edu>
2020-04-24 20:21:15 -0400

    Adding authorization with hash and salt, and DTO with attribute validation

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController: ControllerBase
+    {
+        private IAuthRepository _repo;
+
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult> register(UserForRegisterDtocs userForRegisterDtocs)
+        {
+            //validate
+
+            userForRegisterDtocs.UserName = userForRegisterDtocs.UserName.ToLower();
+            if(await _repo.UserExist(userForRegisterDtocs.UserName))
+                return BadRequest("Username already taken");
+
+            var userToCreate  = new User
+            {
+                UserName = userForRegisterDtocs.UserName
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDtocs.Password);
+
+            return StatusCode(201);
+        }
+
+    }
+}
