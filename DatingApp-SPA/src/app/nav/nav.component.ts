bdb4898ecf1669bd884b56f0fbe21ecf22798ebd
Anushree <as2496@njit.edu>
2020-04-25 12:42:37 -0400

    Angular Service & Communication betweent componet

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { error } from '@angular/compiler/src/util';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+  Login(){
+    this.authService.login(this.model).subscribe(next => {
+        console.log('Logged in successfully!');
+    }, error => {
+      console.log('Failed to login!');
+    });
+  }
+  LoggedIn(){
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+  LogOut(){
+    localStorage.removeItem('token');
+    console.log('Logged out!');
+  }
+}
